96653029482b2fc20b796651755f8785310a4972
tinayxt <xyu100@jh.edu>
2025-12-02 19:58:28 -0500

    Initial commit: added R script and sample dataset.

---
+++ lab_practice.r
@@ -0,0 +1,11 @@
+# GitHub Pre-Lab Practice
+library(tidyverse)
+df <- tibble(
+  x = 1:10,
+  y = x^2
+)
+ggplot(df, aes(x, y)) +
+  geom_line() +
+  theme_minimal()
+write_csv(df, "example_data.csv")
+
